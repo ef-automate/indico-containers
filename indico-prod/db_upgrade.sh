57f2c1efc3bacb1d366187acc4438ea608f696d0
automate <automate@euro-fusion.org>
2024-11-08 10:06:18 +0100

    [TM] fix db_upgrade.sh

--- indico-prod/db_upgrade.sh
+++ indico-prod/db_upgrade.sh
@@ -6,7 +6,7 @@
 docker compose stop
 
 # start up necessary containers and run db upgrade
-docker compose -f docker-compose.yml -f docker-compose.override.yml -f docker-compose.dbupgrade.yml up indico-web
+docker compose -f docker-compose.yml -f docker-compose.override.yml -f docker-compose.dbupgrade.yml up -d indico-web
 
 # start all
 docker compose up -d
