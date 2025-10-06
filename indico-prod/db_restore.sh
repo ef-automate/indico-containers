319f9ae3e60bc2bfc88d4e754f0506de0474a0b9
automate <automate@euro-fusion.org>
2025-10-06 18:16:40 +0200

    update db_restore script

--- indico-prod/db_restore.sh
+++ indico-prod/db_restore.sh
@@ -5,7 +5,7 @@ set -euxo pipefail
 docker compose up -d indico-postgres
 sleep 10
 
-docker compose exec -u postgres indico-postgres zcat /dbdumps/indico.db.sql.gz | psql -U indico --set ON_ERROR_STOP=on indico 
-
-docker compose up -d indico-web
+docker compose exec -u postgres indico-postgres dropdb -U indico --force --if-exists indico
+docker compose exec -u postgres indico-postgres createdb -U indico indico
+docker compose exec -u postgres indico-postgres bash -c "zcat /dbdumps/indico.db.sql.gz | psql -U indico --set ON_ERROR_STOP=on indico"
 
